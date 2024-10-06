fc35db456722c1f44bcda905187ce67c8e803e87
Owais Dashti <owaisdashti@outlook.com>
2024-10-06 13:42:33 +0200

    setting up modules

--- OLA4/monolithic/src/app.module.ts
+++ OLA4/monolithic/src/app.module.ts
@@ -4,15 +4,10 @@ import { AppService } from './app.service';
 import { TrailersModule } from './trailers/trailers.module';
 import { UsersModule } from './users/users.module';
 import { BookingsModule } from './bookings/bookings.module';
-import { UsersModule } from './users/users.module';
-import { TrailersModule } from './trailers/trailers.module';
 import { PaymentsModule } from './payments/payments.module';
-import { PaymentsModule } from './payments/payments.module';
-import { TestModule } from './test/test.module';
-import { BookingsModule } from './bookings/bookings.module';
 
 @Module({
-  imports: [TrailersModule, UsersModule, BookingsModule, PaymentsModule, TestModule],
+  imports: [TrailersModule, UsersModule, BookingsModule, PaymentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- OLA4/monolithic/src/users/dto/create-user.dto.ts
+++ OLA4/monolithic/src/users/dto/create-user.dto.ts
@@ -1,1 +1,38 @@
-export class CreateUserDto {}
+import { IsEmail, IsEnum, IsObject, IsString, Matches } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, {
+    message: 'Password must contain at least one letter and one number',
+  })
+  password: string;
+
+  @IsString()
+  @Matches(/^(0045|\+45|45)?\d{8}$/, {
+    message: 'Phone number must be a danish phone number',
+  })
+  phone: string;
+
+  @IsObject()
+  address: Address;
+
+  @IsEnum(['USER', 'ADMIN'])
+  role: 'USER' | 'ADMIN';
+}
+
+class Address {
+  @IsString()
+  street: string;
+
+  @IsString()
+  city: string;
+
+  @IsString()
+  zip: string;
+}
